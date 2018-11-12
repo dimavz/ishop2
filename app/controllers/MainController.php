dc9187ec0d009acd32669c2256aa7a52dfa3a54d
Dmitry <zatulenko@gmail.com>
2018-11-12 17:00:03 +0300

    Добавлен шаблон главной страницы. Организован вывод модуля брэндов. Сделан бэкап БД.

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -19,5 +19,8 @@ class MainController extends AppController
 		$this->setMeta(App::$app->getProperty('shop_name'),
 			'Главная страница интернет магазина ishop2',
 			'электроника,бытовая техника, компьютеры и периферия');
+		$brands = R::find('brand', 'LIMIT 3');
+//		debug($brands);
+		$this->setData(compact('brands'));
 	}
 }
