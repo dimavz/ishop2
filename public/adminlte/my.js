c4ea94f4e2dad87766de21cbfe5a5a1ba38eed99
Дмитрий <zatulenko@gmail.com>
2020-06-09 13:02:32 +0300

    Часть 3. Добавлен Урок 8.  Управление категориями. Часть 2

--- public/adminlte/my.js
+++ public/adminlte/my.js
@@ -7,3 +7,15 @@ $('.del_category').click(function(){
     var res = confirm('Вы действительно хотите удалить категорию?');
     if(!res) return false;
 });
+
+// Выделение активного пункта меню в админке
+$('.sidebar-menu a').each(function(){
+    var location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+    // console.log(location);
+    var link = this.href;
+    // console.log(link);
+    if(link == location){
+        $(this).parent().addClass('active');
+        $(this).closest('.treeview').addClass('active');
+    }
+});
